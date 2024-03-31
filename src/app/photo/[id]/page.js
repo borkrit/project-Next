ec53a2fc3b91903ec22ae6c104bae6128da1445b
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2024-03-31 13:21:18 +0200

    added modal for img

--- src/app/photo/[id]/page.js
+++ src/app/photo/[id]/page.js
@@ -1,6 +1,12 @@
+import Image from "next/image";
 
-export default function Page (){
+export default function Page ({params:{id}}){
+    console.log(id)
     return(
+        <>
+
         <p>photo page</p>
+            <Image src={`/${id}.jpeg`} alt={'img'} width={300} height={300} />
+        </>
     )
 }
